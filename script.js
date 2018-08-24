55b399b5ca5547017388b0093d51fb20ce537c4b
laissayuri <k.lais.sayuri@gmail.com>
2018-08-24 11:51:12 -0300

    fazer com que a cor do quadradinho seja aleatoria

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
+function aleatorio(min, max){
+   return Math.floor(Math.random() * (max - min +1)) + min;
+}
+
 let quadradinhos = document.querySelectorAll(".quadradinho");
+const cores = ["pink", "lightblue", "aqua", "purple"];
 
 for (quadradinho of quadradinhos){
     quadradinho.onmouseenter = function(){
-        this.style.backgroundColor = "yellow"; //vai pintar a si proprio com o this
+        this.style.backgroundColor = cores[aleatorio(0,3)]; //vai pintar a si proprio com o this
     }
 }
+
